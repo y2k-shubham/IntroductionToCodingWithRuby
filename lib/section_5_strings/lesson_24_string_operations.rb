be4aba9cb4705e4fd5f6d7044e71b985678c2002
Shubham Gupta <ykshubhm@amazon.com>
2024-01-27 12:14:15 +0530

    [section_5] (lesson_24) Try basic string operations

---
+++ lib/section_5_strings/lesson_24_string_operations.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+str = "The quick brown fox jumped over the lazy dog."
+
+puts str.reverse
+puts str.upcase
+puts str.length
+puts str.size
+
+puts str[0..9]
+puts str[-2..-1]
+puts str[4]
